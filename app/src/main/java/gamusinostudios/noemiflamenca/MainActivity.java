9d8431446aeeb5d816568afeb5be3342ec4ec546
Gamusinator <danicano1212@gmail.com>
2018-01-19 14:51:36 +0100

    1

--- app/src/main/java/gamusinostudios/noemiflamenca/MainActivity.java
+++ app/src/main/java/gamusinostudios/noemiflamenca/MainActivity.java
@@ -80,13 +80,13 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_vestidos) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_faldas) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_accesorios) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_contacto) {
 
         }
 
